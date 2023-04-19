56788527dbafbd24e9846bc171a8915db82f527b
Maciej Gierada <maciek.gierada@gmail.com>
2023-04-19 11:47:28 +0200

    feat: update

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,22 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  {
+    import = "astrocommunity.completion.copilot-lua-cmp",
+    -- override the default configuration so that it runs of markdown files
+
+    config = {
+      filetypes = { markdown = true, yaml = true },
+    },
+    --
+  },
+  {
+    import = "astrocommunity.colorscheme.gruvbox",
+  },
+  {
+    import = "astrocommunity.colorscheme.tokyonight",
+  },
+  {
+    import = "astrocommunity.colorscheme.oxocarbon",
+  },
 }
