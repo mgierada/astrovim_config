94376d0be99e3f952b0594f1957081f7f499dd02
Maciej Gierada <maciej@braidwell.io>
2023-05-02 12:39:34 +0200

    feat: fixes

--- plugins/user.lua
+++ plugins/user.lua
@@ -22,7 +22,6 @@ return {
     "pwntester/octo.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      -- "nvim-lua/popup.nvim",
       "nvim-telescope/telescope.nvim",
     },
     -- ops = {},
@@ -30,4 +29,33 @@ return {
     event = "VeryLazy",
   },
   { "ThePrimeagen/harpoon", config = function() require("harpoon").setup {} end },
+  {
+    "andythigpen/nvim-coverage",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("coverage").setup {
+        commands = true, -- create commands
+        highlights = {
+          -- customize highlight groups created by the plugin
+          covered = { fg = "#C3E88D" }, -- supports style, fg, bg, sp (see :h highlight-gui)
+          uncovered = { fg = "#F07178" },
+        },
+        signs = {
+          -- use your own highlight groups or text markers
+          covered = { hl = "CoverageCovered", text = "▎" },
+          uncovered = { hl = "CoverageUncovered", text = "▎" },
+        },
+        summary = {
+          -- customize the summary pop-up
+          min_coverage = 80.0, -- minimum coverage threshold (used for highlighting)
+        },
+        lang = {
+          -- customize language specific settings
+        },
+      }
+    end,
+    event = "VeryLazy",
+  },
 }
