b13db98d920410294e92c3a3267a0f66f47680e2
Maciej Gierada <maciej@braidwell.io>
2023-05-02 11:27:36 +0200

    feat: improve mappings

--- mappings.lua
+++ mappings.lua
@@ -22,9 +22,14 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change scription but the same command
-    ["<leader>a"] = { function() require("harpoon.mark").add_file() end },
-    ["<leader>z"] = { function() require("harpoon.ui").toggle_quick_menu() end },
+    -- harpoon mapping
+    ["<leader>a"] = { function() require("harpoon.mark").add_file() end, desc = "Add mark to harpoon" },
+    ["<leader>z"] = { function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle harpoon UI" },
     ["<tab>"] = { function() require("harpoon.ui").nav_next() end },
+    -- Octo mappings
+    ["<leader>pr"] = { "<cmd> Octo pr list<cr>", desc = "Toggle list of open PRs" },
+    --Neotest mappings
+    ["<leader>ts"] = { function() require("neotest").summary.toggle() end, desc = "Toggle tests summary" },
   },
   t = {
     -- setting a mapping to false will disable it
