b1ea584afa22cf7c127cb77d0afbb874363fadca
Maciej Gierada <maciej@braidwell.io>
2023-05-15 10:34:03 +0200

    feat: fix errors in config by flipping signature and hover off in lsp noice config

--- plugins/user.lua
+++ plugins/user.lua
@@ -116,6 +116,12 @@ return {
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true,
           },
+          hover = {
+            enabled = false,
+          },
+          signature = {
+            enabled = false,
+          },
         },
         -- you can enable a preset for easier configuration
         presets = {
