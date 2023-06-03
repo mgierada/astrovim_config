15f05b945664f2827d73987146072236d421d2b8
Maciej Gierada <maciek.gierada@gmail.com>
2023-06-03 11:24:40 +0200

    feat: add neorg

--- plugins/user.lua
+++ plugins/user.lua
@@ -153,4 +153,27 @@ return {
   {
     "rebelot/kanagawa.nvim",
   },
+
+  --neorg
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
+      }
+    end,
+    event = "VeryLazy",
+  },
 }
