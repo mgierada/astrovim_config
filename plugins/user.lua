ddd38a4a9b9e2b16e36a570284c31fe43c58e9ce
Maciej Gierada <maciek.gierada@gmail.com>
2023-06-23 20:26:55 +0200

    feat: add flutter dev tools

--- plugins/user.lua
+++ plugins/user.lua
@@ -176,4 +176,16 @@ return {
     end,
     event = "VeryLazy",
   },
+
+  -- flutter lsp and tools
+  {
+    "akinsho/flutter-tools.nvim",
+    -- lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = function() require("flutter-tools").setup {} end,
+    ft = { "dart" },
+  },
 }
