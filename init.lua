96ce0da6ba565a079203c64e309853580b2abec4
Maciej Gierada <maciej@braidwell.io>
2023-07-08 10:19:23 +0200

    feat: nightfly as a defalut colorscheme

--- init.lua
+++ init.lua
@@ -45,7 +45,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "onedark_dark",
+  colorscheme = "nightfly",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
