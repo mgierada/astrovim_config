db8fbe16f238041fd2024205002a1dc9843a094a
Maciej Gierada <maciek.gierada@gmail.com>
2023-08-23 08:53:15 +0200

    feat: use community version of gitworktree

--- plugins/user.lua
+++ plugins/user.lua
@@ -327,4 +327,12 @@ return {
       "mfussenegger/nvim-dap",
     },
   },
+
+  -- nvim worktree
+  {
+    "/brandoncc/git-worktree.nvim",
+    event = "VeryLazy",
+    config = function() require("telescope").load_extension "git_worktree" end,
+    branch = "catch-and-handle-telescope-related-error",
+  },
 }
