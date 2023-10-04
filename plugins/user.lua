5a58208c02dc201fc3524c6852983c7fe4dc6fb8
Maciej Gierada <maciej@braidwell.io>
2023-10-04 17:17:00 +0200

    feat: overwrite hairline

--- plugins/user.lua
+++ plugins/user.lua
@@ -405,14 +405,6 @@ return {
     event = "VeryLazy",
   },
 
-  -- -- Pybumper
-  -- {
-  --   dir = "/Users/maciej/00_codes/pybumper.git/main",
-  --   dependencies = { "MunifTanjim/nui.nvim" },
-  --   config = function() require("pybumper").setup { {} } end,
-  --   dev = true,
-  --   event = "VeryLazy",
-  -- },
   -- Pybumper
   {
     dir = "/Users/maciej/00_codes/pybumper.git/main",
@@ -422,4 +414,37 @@ return {
     event = "VeryLazy",
   },
 
+  --hairline
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require "astronvim.utils.status"
+      local package_info = require "package-info"
+      -- Get the status from your custom component
+      local custom_component_status = package_info.get_status()
+      opts.statusline = { -- statusline
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+        status.component.git_branch(),
+        status.component.file_info { filetype = {}, filename = false, file_modified = false },
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        status.component.lsp(),
+        status.component.treesitter(),
+        status.component.nav(),
+        -- Add your custom component
+        {
+          text = custom_component_status,
+          hl = { fg = "custom_fg_color", bg = "custom_bg_color" }, -- Adjust colors as needed
+          padding = { left = 1, right = 1 }, -- Adjust padding as needed
+        },
+      }
+
+      -- return the final configuration table
+      return opts
+    end,
+  },
 }
