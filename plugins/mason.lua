3e4aa31775f9331872d60478695c270ce9599e3b
Maciej Gierada <maciej@braidwell.io>
2023-10-17 10:46:05 +0200

    feat: add handlers for mason-lspconfig

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,4 +1,30 @@
 -- customize mason plugins
+local handlers = {
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {}
+  end,
+  -- Next, you can provide targeted overrides for specific servers.
+  -- ["rust_analyzer"] = function() require("rust-tools").setup {} end,
+  ["lua_ls"] = function()
+    local lspconfig = require "lspconfig"
+    lspconfig.lua_ls.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+          hint = {
+            enable = true,
+          },
+        },
+      },
+    }
+  end,
+}
+
 return {
   -- use mason-lspconfig to configure LSP installations
   {
@@ -6,6 +32,7 @@ return {
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
       ensure_installed = { "lua_ls", "tsserver", "pyright", "bashls" },
+      handlers = handlers,
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
