f1e6c4fc839b41cd753afbe2114848c5e24808b8
Maciej Gierada <maciej@braidwell.io>
2023-11-30 16:23:24 +0100

    feat: remove filetype plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -518,18 +518,18 @@ return {
   --   end,
   -- },
   --
-  {
-    "nathom/filetype.nvim",
-    config = function()
-      require("filetype").setup {
-        overrides = {
-          extensions = {
-            tf = "terraform",
-            tfvars = "terraform",
-            tfstate = "json",
-          },
-        },
-      }
-    end,
-  },
+  -- {
+  --   "nathom/filetype.nvim",
+  --   config = function()
+  --     require("filetype").setup {
+  --       overrides = {
+  --         extensions = {
+  --           tf = "terraform",
+  --           tfvars = "terraform",
+  --           tfstate = "json",
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
 }
