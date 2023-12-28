7fdf0f20a62218e9abcda045b5b11c447e177a02
Maciej Gierada <maciek.gierada@gmail.com>
2023-12-28 10:31:36 +0100

    feat: add colorscheme

---
+++ plugins/colorschemes.lua
@@ -0,0 +1,34 @@
+return {
+  -- tokyonight colorscheme
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
+  -- kangawa colorscheme
+  {
+    "rebelot/kanagawa.nvim",
+  },
+
+  -- everforest colorscheme
+  {
+    "sainnhe/everforest",
+  },
+
+  -- nightfly colorscheme
+  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
+
+  -- moonflight colorscheme
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+
+  -- catppuccin colorscheme
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- onedarkpro colorscheme
+  {
+    "olimorris/onedarkpro.nvim",
+    -- priority = 1000 -- Ensure it loads first
+  },
+}
