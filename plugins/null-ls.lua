c374032709e9910806fd0a0fde0ecf1a703033fc
Maciej Gierada <maciek.gierada@gmail.com>
2023-12-28 10:31:30 +0100

    feat: replace null-ls with none-ls

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,5 +1,5 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
+  "nvimtools/none-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
     local null_ls = require "null-ls"
@@ -9,8 +9,8 @@ return {
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier,
       null_ls.builtins.formatting.black,
       -- null_ls.builtins.formatting.isort,
     }
