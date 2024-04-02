b5c644051eb463e7cdce08d064908133344aa06c
Maciej Gierada <maciej@braidwell.io>
2024-04-02 16:34:25 +0200

    feat: add comunity plugin

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,19 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{
+		import = "astrocommunity.completion.copilot-lua-cmp",
+		-- override the default configuration so that it runs of markdown files
+
+		config = {
+			filetypes = { markdown = true, yaml = true },
+		},
+		--
+	},
+	-- import/override with your plugins folder
 }
