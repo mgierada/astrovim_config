91696604198b4c3f4466b41c337c0e3e9b18833c
Maciej Gierada <maciek.gierada@gmail.com>
2024-04-03 09:09:36 +0200

    Moving to astronvim v4 (#1)
    * feat: moving to astronvim v4
    
    * feat: make sure all plugins are added correctly
    
    * feat: add comunity plugin
    
    * feat: use better escape

---
+++ lua/community.lua
@@ -0,0 +1,19 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{
+		import = "astrocommunity.completion.copilot-lua-cmp",
+		-- override the default configuration so that it runs of markdown files
+
+		config = {
+			filetypes = { markdown = true, yaml = true },
+		},
+		--
+	},
+	-- import/override with your plugins folder
+}
