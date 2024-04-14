da6db44a978b81396c1f938908e7a84f47205783
Maciej Gierada <maciej.gierada@sofomo.com>
2024-04-14 13:30:16 +0200

    feat: use nightly branch in neogit plugins because it supports nvim v10

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,12 +1,13 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
-    -- Only one of these is needed, not both.
-    "nvim-telescope/telescope.nvim", -- optional
-    "ibhagwan/fzf-lua", -- optional
-  },
-  config = true,
-  event = "VeryLazy",
+	"NeogitOrg/neogit",
+	branch = "nightly", -- provides support for nvim v.10
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+		-- Only one of these is needed, not both.
+		"nvim-telescope/telescope.nvim", -- optional
+		"ibhagwan/fzf-lua", -- optional
+	},
+	config = true,
+	event = "VeryLazy",
 }
