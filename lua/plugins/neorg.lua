7371ac8cbc15095beeb0e1e9e1081ebbaa917519
Maciej Gierada <maciej@braidwell.io>
2024-04-15 08:58:33 +0200

    feat: update neorg config to 8.0.0

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,24 +1,23 @@
 return {
-  --neorg
-  {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-            },
-          },
-        },
-      }
-    end,
-    event = "BufRead",
-  },
+	--neorg
+	{
+		"nvim-neorg/neorg",
+		dependencies = { "luarocks.nvim" },
+		config = function()
+			require("neorg").setup({
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/notes",
+							},
+						},
+					},
+				},
+			})
+		end,
+		event = "BufRead",
+	},
 }
