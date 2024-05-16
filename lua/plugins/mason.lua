0388f2e38027cca20613c310a8202d625643a885
Maciej Gierada <maciek.gierada@gmail.com>
2024-05-16 10:11:53 +0200

    feat: add go-lang config updates

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -17,6 +17,7 @@ return {
         "rust_analyzer",
         "ruff_lsp",
         "pyright",
+        "gopls",
       })
     end,
   },
@@ -27,7 +28,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed =
-        require("astrocore").list_insert_unique(opts.ensure_installed, { "prettier", "stylua", "ruff" })
+          require("astrocore").list_insert_unique(opts.ensure_installed, { "prettier", "stylua", "ruff" })
     end,
   },
   {
