ea4fc3fed4c099a7a3dbbe9340d91f64abad9881
Maciej Gierada <maciek.gierada@gmail.com>
2024-06-03 20:37:07 +0200

    feat: add vimdadbod

---
+++ lua/plugins/dadpodui.lua
@@ -0,0 +1,21 @@
+--dadbodui
+return {
+	-- VimDadBodUi
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "mgierada/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+	},
+}
