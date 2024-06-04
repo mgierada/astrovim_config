a7dc739e03ede5f7104911bd18eebd1d7b01774d
Maciej Gierada <maciej@braidwell.io>
2024-06-04 11:15:00 +0200

    feat: update neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -2,11 +2,11 @@ return {
 	"NeogitOrg/neogit",
 	-- branch = "nightly", -- provides support for nvim v.10
 	dependencies = {
-		"nvim-lua/plenary.nvim", -- required
-		"sindrets/diffview.nvim", -- optional - Diff integration
+		"nvim-lua/plenary.nvim",       -- required
+		"sindrets/diffview.nvim",      -- optional - Diff integration
 		-- Only one of these is needed, not both.
 		"nvim-telescope/telescope.nvim", -- optional
-		"ibhagwan/fzf-lua", -- optional
+		"ibhagwan/fzf-lua",            -- optional
 	},
 	config = true,
 	event = "VeryLazy",
